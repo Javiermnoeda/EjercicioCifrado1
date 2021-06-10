b6b812634bc681eb98ce49f8627d44417ac5c2c2
Javier <jmnoeda2000@gmail.com>
2021-06-10 13:53:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciocifrado/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.ejerciciocifrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ejerciciocifrado.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import javax.crypto.BadPaddingException
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+import kotlin.jvm.Throws
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding :ActivityMainBinding
+
+    //Para declarar constantes que no van a cambiar y tb para que se pueda acceder a ellas desde cualquier parte del programa
+    companion object {
+        const val LLAVE_EN_STRING : String = "__Moway19312"
+        const val TIPO_DE_CIFRADO : String = "AES/ECB/PKCS5Padding"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botonCifrado.setOnClickListener {
+            val textoCifrado = cifrar(""+binding.editText.text)
+            binding.textviewCrifrado.text=textoCifrado
+        }
+
+        binding.botonDescifrado.setOnClickListener {
+            val textoDescifrado = descifrar(""+binding.textviewCrifrado.text)
+            binding.textviewDescifrado.text=textoDescifrado
+        }
+    }
+}
+
+private fun cifrar(textoParaCifrar : String) : String {
+    val cipher = Cipher.getInstance(MainActivity.TIPO_DE_CIFRADO)
+    cipher.init(Cipher.ENCRYPT_MODE, getKey(MainActivity.LLAVE_EN_STRING))
+    return textoParaCifrar
+}
+
+@Throws(BadPaddingException::class)
+private fun descifrar(textoCifrado : String) : String{
+    val cipher = Cipher.getInstance(MainActivity.TIPO_DE_CIFRADO)
+    cipher.init(Cipher.DECRYPT_MODE, getKey(MainActivity.LLAVE_EN_STRING))
+    return textoCifrado
+}
+
+private fun getKey(llaveEnString: String): SecretKeySpec {
+    var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+    val sha = MessageDigest.getInstance("SHA-1")
+    llaveUtf8 = sha.digest(llaveUtf8)
+    llaveUtf8 = llaveUtf8.copyOf(10)
+    return SecretKeySpec(llaveUtf8,"AES")
+}
+
